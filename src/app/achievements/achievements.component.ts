05053fd89e25fffdfbaaa5b763ea74e7ae496bd5
Imran Ali <152004405+imranali2112@users.noreply.github.com>
2025-03-04 16:53:18 +0500

    new changes

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './achievements.component.scss'
 })
 export class AchievementsComponent {
+  achievementsData: any[]= [
+    {
+      image:'assets/images/member.svg',
+      members:'2,245,341',
+      achievementsName:'Member'
+    },
+    {
+      image:'assets/images/clubs.png',
+      members:'46,328',
+      achievementsName:'Clubs'
+    },
+    {
+      image:'assets/images/booking.svg',
+      members:'828,867',
+      achievementsName:'Events Booking'
+    },
+    {
+      image:'assets/images/payments.svg',
+      members:'1,926,436',
+      achievementsName:'Payments'
+    },
+  ]
 
 }
